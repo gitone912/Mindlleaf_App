ef9447e35b3df524c8f3071d54cbd202085ab9ae
gitone912 <akash.101@kvsroranchi.in>
2025-01-20 23:38:24 +0530

    remove category

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-import categoryReducer from "./slices/categorySlice";
+
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
-    categories: categoryReducer,
+    auth: authReducer
   },
 });
 
